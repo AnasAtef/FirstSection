6f8e68b7833f0700ea99db761b899af648f0b193
AnasAtef <anasbasha1212@gmail.com>
2025-04-23 08:42:22 +0200

    Added Countries Endpoints

--- FirstSection/Program.cs
+++ FirstSection/Program.cs
@@ -1,29 +1,30 @@
 using FirstSection.Data;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-var connectionString= builder.Configuration.GetConnectionString("HotelListingDbConnectionString");
+// Add services to the container
+var connectionString = builder.Configuration.GetConnectionString("HotelListingDbString");
 builder.Services.AddDbContext<HotelListingDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("HotelListingDbString")));
+    options.UseSqlServer(connectionString));
+
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(); // <-- This is necessary for Swagger UI to work
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
